440c98f1befea03b6851a9ebc5c8c12b1cc4fbf4
Jim Andreasen <jim@andreasen.dev>
2026-07-19 18:27:21 -0400

    need a failed checkov scan for a red/green refactor

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # job-templates
 
 Will write tests later. Can't get so mired in yak shedding that I don't get anything done!
+
+Note: How to quickly run a checkov locally:
+
+```bash
+	docker run -it --rm -v "$1(pwd):/src" -w /src --entrypoint="" bridgecrew/checkov:latest sh -c "checkov --directory /src"
+```
---
+++ tests/checkov-fail/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws    = { source = "hashicorp/aws" }
+    random = { source = "hashicorp/random" }
+  }
+  backend "s3" {
+    encrypt      = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_uuid" "bucket_suffix" {
+}
+
+locals {
+  bucket_rando_suffix = substr(random_uuid.bucket_suffix.result, 0, 8)
+}
+
+# Checkov is pretty unhappy with this bucket
+# Check: CKV_AWS_21: "Ensure all data stored in the S3 bucket have versioning enabled"
+# Check: CKV_AWS_18: "Ensure the S3 bucket has access logging enabled"
+# Check: CKV_AWS_144: "Ensure that S3 bucket has cross-region replication enabled"
+# Check: CKV_AWS_145: "Ensure that S3 buckets are encrypted with KMS by default"
+# Check: CKV2_AWS_6: "Ensure that S3 bucket has a Public Access block"
+# Check: CKV2_AWS_61: "Ensure that an S3 bucket has a lifecycle configuration"
+# Check: CKV2_AWS_62: "Ensure S3 buckets should have event notifications enabled"
+resource "aws_s3_bucket" "this" {
+  bucket = "job-template-test-bucket-${bucket_rando_suffix}"
+}
